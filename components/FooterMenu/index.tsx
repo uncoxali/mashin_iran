4ac1807524797cad7695b72ed93c4a08edd75786
unknown <alif.mohamady20@gmail.com>
2021-08-24 15:06:55 +0430

    add page exhibition and add menu

--- components/FooterMenu/index.tsx
+++ components/FooterMenu/index.tsx
@@ -3,12 +3,14 @@ import PlusIcon from '@assets/svg/Plus.svg';
 
 const index = () => {
     return (
-        <div className="flex justify-center">
-            <div className=" bottom-3 fixed">
-                <div className=" h-14 w-44 bg-green-600 rounded-full flex justify-center items-center">
-                    <div className="flex  text-white text-sm px-5">
-                        <PlusIcon className="ml-5 w-7 h-8" />
-                        <p className="mt-2">افزودن آگهی</p>
+        <div className="lg:hidden md:hidden block">
+            <div className="flex justify-center ">
+                <div className=" bottom-3 fixed">
+                    <div className=" h-14 w-44 bg-green-600 rounded-full flex justify-center items-center">
+                        <div className="flex  text-white text-sm px-5">
+                            <PlusIcon className="ml-5 w-7 h-8" />
+                            <p className="mt-2">افزودن آگهی</p>
+                        </div>
                     </div>
                 </div>
             </div>
